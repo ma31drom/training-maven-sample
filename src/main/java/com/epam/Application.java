2dbbc2f474d7f274d6ba575a5e8f2077dfdbe934
Maxim Naumovich <MNaumovich@inspirebrands.com>
2021-09-27 20:42:02 +0300

    Something else

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -5,6 +5,9 @@ import java.util.logging.Logger;
 
 public class Application {
 
+    /*
+    Something!
+     */
     public static void main(String[] args) {
         System.out.println("Hello java!");
         Logger.getLogger("basic").info("Hello logger!");
