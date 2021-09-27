aab695f32626910b7bf7698e555245760021bc0a
ma31drom <ma31drom@tut.by>
2021-09-27 20:41:17 +0300

    Update Application.java

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -5,6 +5,9 @@ import java.util.logging.Logger;
 
 public class Application {
 
+    /*
+    * Cool clazz
+    */
     public static void main(String[] args) {
         System.out.println("Hello java!");
         Logger.getLogger("basic").info("Hello logger!");
